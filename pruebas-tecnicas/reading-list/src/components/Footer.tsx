04f8db4c825a70e9b9ed76e523d31657495f624d
Omar Lengua <omarns21@gmail.com>
2024-09-29 14:28:13 +0200

    proyecto eliminado, comienzo otra vez

--- pruebas-tecnicas/reading-list/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer: React.FC = () => {
-  return (
-    <footer className="bg-blue-600 p-4 text-center">
-      <p className="text-white">&copy; 2024 My E-commerce Site</p>
-    </footer>
-  );
-};
