60020dcce9ef04f1aa44f51e718446369931cd72
Omar Lengua <omarns21@gmail.com>
2024-10-20 00:21:31 +0200

    Feat: implementation in comp. videocarousel

---
+++ iphone-page/src/components/Model.jsx
@@ -0,0 +1,16 @@
+const Model = () => {
+
+  return (
+    <div className="common-padding">
+        <div className="screen-max-width">
+            <h1 id="heading" className="section-heading">
+                Take a closer look.
+            </h1>
+
+        </div>
+
+    </div>      
+  )
+}
+
+export default Model;
